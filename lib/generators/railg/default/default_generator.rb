ccbe95e42e61f3f255505265a63330eef9ba097e
nishisuke <nishisuke13+git@gmail.com>
2018-11-17 15:17:20 +0900

    Add default generator

---
+++ lib/generators/railg/default/default_generator.rb
@@ -0,0 +1,25 @@
+module Railg
+  class DefaultGenerator < ::Rails::Generators::Base
+    def configure_generator
+      environment <<-EOT, env: 'development'
+  config.generators do |g|
+    g.assets false
+    g.helper false
+    g.jbuilder false
+    g.test_framework false
+  end
+      EOT
+    end
+
+    def insert_meta_tag
+      insert_into_file 'app/views/layouts/application.html.erb', <<-EOT, after: "  <head>\n"
+  <meta charset='UTF-8'>
+  <meta name='viewport' content='width=device-width,initial-scale=1'>
+      EOT
+    end
+
+    def create_db
+      rake 'db:create'
+    end
+  end
+end
